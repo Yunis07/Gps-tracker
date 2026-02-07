24d1d3f71e4ee558f4662fbb45690619fbea0533
Yunis <classanoymous@gmail.com>
2026-02-07 22:41:14 +0530

    Refactor service worker caching logic

--- sw.js
+++ sw.js
@@ -9,20 +9,6 @@ const urlsToCache = [
     'https://cdn.jsdelivr.net/npm/@turf/turf@6.5.0/turf.min.js'
 ];
 
-// Install service worker
 self.addEventListener('install', (event) => {
     event.waitUntil(
-        caches.open(CACHE_NAME).then((cache) => {
-            return cache.addAll(urlsToCache);
-        })
-    );
-});
-
-// Fetch from cache or network
-self.addEventListener('fetch', (event) => {
-    event.respondWith(
-        caches.match(event.request).then((response) => {
-            return response || fetch(event.request);
-        })
-    );
-});
+        caches.open(CACHE_NAME).then
